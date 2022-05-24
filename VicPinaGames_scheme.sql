6775aa1fe26da6f7fb0f341a6225cfab8ae7a509
Vic Piña <vicpanzu7@gmail.com>
2022-05-24 13:51:58 -0500

    SQL Scheme

---
+++ VicPinaGames_scheme.sql
@@ -0,0 +1,67 @@
+/* SQL -> Structured Query Language */
+
+CREATE DATABASE IF NOT EXISTS VicPinaGames;
+
+USE VicPinaGames;
+
+CREATE TABLE Genres(
+    genre_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    genre_name VARCHAR(50) NOT NULL
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+
+CREATE TABLE Disabilities(
+    disability_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    disability_name VARCHAR(50) NOT NULL
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+
+CREATE TABLE Games(
+    game_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    game_title VARCHAR(255) UNIQUE NOT NULL,
+    download_link VARCHAR(255) UNIQUE,
+    release_date DATE NOT NULL,
+    game_description TEXT NOT NULL,
+    game_genre VARCHAR (255) NOT NULL,
+    trailer VARCHAR(255)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+
+CREATE TABLE Users(
+    user_id VARCHAR(50) UNSIGNED PRIMARY KEY,
+    user_email VARCHAR(50) UNIQUE NOT NULL,
+    age_range ENUM('4-10', '10-18', '+18'),
+    user_disability INTEGER UNSIGNED,
+    FOREIGN KEY(user_disability)
+     REFERENCES Disabilities(disability_id)
+     ON DELETE RESTRICT
+     ON UPDATE CASCADE
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+
+CREATE TABLE Comments(
+    comment_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    comment_date DATETIME NOT NULL,
+    comment_body TEXT NOT NULL,
+    game_id_c INTEGER UNSIGNED NOT NULL,
+    user_id_c VARCHAR(50) NOT NULL,
+    FOREIGN KEY(game_id_c)
+     REFERENCES Games(game_id)
+     ON DELETE RESTRICT
+     ON UPDATE CASCADE
+    FOREIGN KEY(user_id_c)
+     REFERENCES Users(user_id)
+     ON DELETE RESTRICT
+     ON UPDATE CASCADE
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+
+CREATE TABLE Likes(
+    like_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    like_date DATETIME NOT NULL,
+    game_id_l INTEGER UNSIGNED NOT NULL,
+    user_id_l VARCHAR(50) NOT NULL,
+    FOREIGN KEY(game_id_l)
+     REFERENCES Games(game_id)
+     ON DELETE RESTRICT
+     ON UPDATE CASCADE
+    FOREIGN KEY(user_id_l)
+     REFERENCES Users(user_id)
+     ON DELETE RESTRICT
+     ON UPDATE CASCADE
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
